8ef7439fc81e7c50a783532ab655d3fa82e8d702
Daniel Benusovich <dbenusovich@gmail.com>
2018-04-22 00:27:32 -0700

    Project complete over router
    Further work required on local connection operation. Needs comments

--- LEDRCM3200/LEDRCM3200.C
+++ LEDRCM3200/LEDRCM3200.C
@@ -2,8 +2,39 @@
 
 #define PORTA_AUX_IO
 
-#memmap xmem
 #use rcm3200.lib
+
+/*
+ * Pick the predefined TCP/IP configuration for this sample.  See
+ * LIB\TCPIP\TCP_CONFIG.LIB for instructions on how to set the
+ * configuration.
+ */
+#define TCPCONFIG 7
+#define USE_RABBITWEB 1
+
+/*
+ * TCP/IP modification - reduce TCP socket buffer
+ * size, to allow more connections. This can be increased,
+ * with increased performance, if the number of sockets
+ * are reduced.  Note that this buffer size is split in
+ * two for TCP sockets--1024 bytes for send and 1024 bytes
+ * for receive.
+ */
+#define TCP_BUF_SIZE 2048
+
+/*
+ * Web server configuration
+ */
+
+/*
+ * Define the number of HTTP servers and socket buffers.  Note
+ * that we need one TCP socket for SMTP support. With
+ * tcp_reserveport(), fewer HTTP servers are needed.
+ */
+#define HTTP_MAXSERVERS 2
+#define MAX_TCP_SOCKET_BUFFERS 3
+
+#define REDIRECTHOST "192.168.0.101"
 
 #define OFF 0
 #define ON 1
@@ -13,12 +44,87 @@
 #define LIGHT_LEFT_EW 3
 #define LIGHT_RIGHT_EW 4
 
-fontInfo fi6x8, fi8x10;
+#define REDIRECTTO      "http://" REDIRECTHOST ""
+
+#memmap xmem
+
+#use "dcrtcp.lib"
+#use "http.lib"
+
+#ximport "C:/Users/db217620/Repositories/COMP598/LEDRCM3200/testSendNumber.html"   index_html
+
+
+
+//BEGIN DEFINITIONS
+
+/* the default for / must be first */
+const HttpType http_types[] =
+{
+   { ".shtml", "text/html", zhtml_handler}, // ssi
+   { ".html", "text/html", NULL},           // html
+   { ".cgi", "", NULL},                     // cgi
+   { ".gif", "image/gif", NULL}
+};
+
+typedef struct {
+	char *name;
+	char *value;
+} FORMType;
 
 enum directions{
     NS = 0,
     EW = 1
 };
+
+//END DEFINITIONS
+
+
+//BEGIN GLOBAL VARIABLES
+FORMType FORMSpec;
+fontInfo fi6x8, fi8x10;
+int currentSpeed;
+#web currentSpeed;
+//END GLOBAL VARIABLES
+
+int parsePost(HttpState *state){
+    int retval;
+    retval = sock_aread(&state->s, state->p, (state->content_length < TCP_BUF_SIZE-1)?(int)state->content_length:TCP_BUF_SIZE-1);
+
+    if(retval < 0) return 1;
+
+    state->subsubstate += retval;
+
+    if (state->subsubstate >= state->content_length) {
+        state->buffer[(int)state->content_length] = '\0';
+        http_scanpost(FORMSpec.name, state->buffer, FORMSpec.value, 20);
+        return 1;
+    }
+    return 0;
+}
+
+int Submit(HttpState* state){
+	if (state->length) {
+		/* buffer to write out */
+		if (state->offset < state->length) {
+			state->offset += sock_fastwrite(&state->s,
+					state->buffer + (int)state->offset,
+					(int)state->length - (int)state->offset);
+		} else {
+			state->offset = 0;
+			state->length = 0;
+		}
+    } else {
+        FORMSpec.value[0] = '\0';
+
+        state->p = state->buffer;
+        if(parsePost(state)){
+            currentSpeed = atoi(FORMSpec.value);
+        }
+    }
+    state->substate = 0;
+    cgi_redirectto(state,REDIRECTTO);
+    return 0;
+}
 
 void stopSignal(int direction){
     if(direction == 0){
@@ -60,20 +166,41 @@ void advancedGreenSignal(int direction){
     }
 }
 
+
+
+
+//Required to be defined before main due to requirements for function callbacks
+const HttpSpec http_flashspec[] =
+{
+   { HTTPSPEC_FILE, 		"/",				index_html,    NULL, 	0, NULL, NULL},
+   { HTTPSPEC_FILE, 		"/index.html",	index_html,    NULL, 	0, NULL, NULL},
+   { HTTPSPEC_FUNCTION,	"/parsenumber.cgi",	0,  		Submit,	0, NULL, NULL}
+};
+
 void main(){
 
-    static int currentState;
-    static int stopped;
-    static int running;
-    static int delayTime;
+    int currentState;
+    int stopped;
+    int running;
+    char data[20];
 
+    //Inital values for data packet -- this must be done
+    FORMSpec.name = "numerical_input";
+    FORMSpec.value = data;
+    currentSpeed = 500;
+
+    //Initial states for variables
     currentState = 0;
-    delayTime = 500;
     stopped = NS;
     running = EW;
 
+    //init system
     brdInit();
     dispInit();
+
+    sock_init();
+    http_init();
+    tcp_reserveport(80);
 
     glBackLight(1);
 	 glXFontInit(&fi6x8, 6, 8, 32, 127, Font6x8);          // Initialize 6x8 font
@@ -85,6 +212,7 @@ void main(){
     glPrintf (0,  16, &fi6x8,  "Hi Ed");
 
     for(;;){
+        http_handler();
         costate{
         switch(currentState){
             case 0:
@@ -115,8 +243,8 @@ void main(){
 					 }
 				}
 		  		currentState = (currentState + 1) % 4;
-            printf("%d\n", currentState);
-            waitfor(DelayMs(delayTime));
+            //printf("%d\n", currentState);
+            waitfor(DelayMs(currentSpeed));
         }
     }
 }
